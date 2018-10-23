2ab94de54ab28848156c9079411df15ab5d213d9
jloh <git@jloh.co>
2018-10-23 22:15:50 +1100

    Start http status website

---
+++ .eleventy.js
@@ -0,0 +1,28 @@
+
+const CleanCSS = require("clean-css");
+
+module.exports = function(config) {
+
+  // just pass our redirect and header rules through to the dist folder
+  config.addPassthroughCopy("_redirects");
+  config.addPassthroughCopy("_headers");
+
+  config.addFilter("cssmin", function(code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
+
+  return {
+
+    // what goes where?
+    dir: {
+      input: "src",
+      output: "dist",
+      includes: "_includes"
+    },
+
+    // some handy options
+    templateFormats : ["njk"],
+    passthroughFileCopy: true
+
+  };
+};
