cd3398333bf682de3e394157741994998c51d53e
jloh <git@jloh.co>
2018-11-06 18:24:37 +1100

    Allow indexing

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,6 @@ module.exports = function (config) {
   // just pass our redirect and header rules through to the dist folder
   config.addPassthroughCopy("_redirects");
   config.addPassthroughCopy("_headers");
-  config.addPassthroughCopy("robots.txt");
   config.addPassthroughCopy("src/js");
   config.addPassthroughCopy("src/css");
 
