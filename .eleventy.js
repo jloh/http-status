6075d5ba0877bfd0d8f37804351a995f2ec4276e
James Loh <git@jloh.co>
2020-01-04 16:05:17 +1100

    Start tailwind redo

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 const cacheBuster = require('@mightyplow/eleventy-plugin-cache-buster');
+// const pluginRss = require("@11ty/eleventy-plugin-rss");
+// const { DateTime } = require("luxon");
 
 module.exports = function (config) {
 
@@ -14,6 +16,18 @@ module.exports = function (config) {
   };
   config.addPlugin(cacheBuster(cacheBusterOptions));
 
+  // RSS plugin is required for our sitemap?
+  config.addPlugin(pluginRss);
+
+  // Required for the above
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  // config.addFilter('htmlDateString', (dateObj) => {
+  //   return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+  // });
+
+  // Don't ignore codes.json
+  config.setUseGitIgnore(false);
+
   return {
 
     // what goes where?
@@ -25,6 +39,8 @@ module.exports = function (config) {
 
     // some handy options
     templateFormats: ["njk"],
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
     passthroughFileCopy: true
 
   };
