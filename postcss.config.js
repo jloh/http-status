9fe25535fc62659d3d034dbe3284fe6e6361d0a5
James Loh <git@jloh.co>
2020-01-25 22:44:03 +1100

    Fix postcss build
    without this PostCSS fails to find the layouts folder and errors out

--- postcss.config.js
+++ postcss.config.js
@@ -6,7 +6,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
 
   // Specify the paths to all of the template files in your project
   content: [
-    'layouts/',
+    currentDir + 'layouts/',
   ],
   whitelist: [
     'pre'
